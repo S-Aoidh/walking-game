b754e52db7c5b83a155c63f9e50d6c6bd59a3c1c
bracu <braculations@gmail.com>
2024-09-10 22:15:50 +1000

    add animation

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div class="trees-left" :style="{ backgroundPositionY: `${scrollPosition}px` }"></div>
     <div class="trees-right" :style="{ backgroundPositionY: `${scrollPosition}px` }"></div>
     <div class="time-display">Time: {{ seconds }}s</div>
+    <div class="moving-block" :style="{ animationDuration: '5s', width: `${blockSize}px`, height: `${blockSize}px` }"></div>
   </div>
 </template>
 
@@ -13,7 +14,8 @@ export default {
     return {
       seconds: 0,
       scrollPosition: 0,
-      scrollSpeed: 2, // Adjust scrolling speed as needed
+      scrollSpeed: 2,
+      blockSize: 50, // Initial size of the block
       intervalId: null,
     };
   },
@@ -27,10 +29,11 @@ export default {
     startScrolling() {
       this.intervalId = setInterval(() => {
         this.seconds += 1;
-        this.scrollPosition += this.scrollSpeed; // Move forward
-        if (this.scrollPosition >= 100) { // Adjust if necessary
-          this.scrollPosition = 0; // Reset position
+        this.scrollPosition += this.scrollSpeed;
+        if (this.scrollPosition >= 100) {
+          this.scrollPosition = 0;
         }
+        this.blockSize += 10; // Increase the block size over time
       }, 1000); // Update every second
     },
   },
@@ -43,25 +46,30 @@ export default {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  background-color: #111; /* Dark background */
+  color: white; /* Light text color */
 }
 
 .road {
   position: absolute;
-  left: 0;
-  top: 50%;
-  width: 100%;
-  height: 50px;
+  left: 50%;
+  top: 0;
+  width: 50px;
+  height: 100vh;
   background: gray;
-  transform: translateY(-50%);
-  rotate: 1;
+  transform: translateX(-50%);
+  clip-path: polygon(0 0, 100% 0, 60% 100%, 40% 100%);
+  z-index: 1;
 }
 
 .trees-left, .trees-right {
   position: absolute;
   top: 0;
-  width: 200px; /* Adjust width as needed */
-  height: 100px;
-  background-color: hotpink;}
+  width: 200px;
+  height: 100%;
+  background-color: #333; /* Darker background for trees */
+  background-repeat: repeat-y;
+}
 
 .trees-left {
   left: 0;
@@ -69,7 +77,7 @@ export default {
 
 .trees-right {
   right: 0;
-  background: url('https://www.vectorstock.com/royalty-free-vector/simple-tree-vector-164659') repeat-y; /* Add your tree texture image */
+  background: url('https://www.vectorstock.com/royalty-free-vector/simple-tree-vector-164659') repeat-y;
 }
 
 .time-display {
@@ -77,9 +85,17 @@ export default {
   bottom: 10px;
   left: 10px;
   font-size: 24px;
-  color: white;
   background: rgba(0, 0, 0, 0.5);
   padding: 10px;
+}
+
+.moving-block {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  background-color: hotpink;
+  transform: translateX(-50%);
+  animation: moveDown 5s linear infinite, growBlock 5s linear infinite; /* Animate both movement and growth */
 }
 
 @keyframes moveDown {
@@ -91,4 +107,14 @@ export default {
   }
 }
 
+@keyframes growBlock {
+  0% {
+    width: 50px; /* Initial size */
+    height: 50px; /* Initial size */
+  }
+  100% {
+    width: 100px; /* Final size */
+    height: 100px; /* Final size */
+  }
+}
 </style>
